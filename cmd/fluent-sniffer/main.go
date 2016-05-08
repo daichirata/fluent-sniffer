778a41a68d3a5d586d0633507f350dd2a17b8538
Daichi HIRATA <bunny.hop.md@gmail.com>
2016-05-09 02:39:54 +0900

    dump

--- cmd/fluent-sniffer/main.go
+++ cmd/fluent-sniffer/main.go
@@ -1,1 +1,42 @@
 package main
+
+import (
+	"bytes"
+	"fmt"
+	"log"
+
+	"github.com/google/gopacket"
+	"github.com/google/gopacket/pcap"
+	"github.com/msgpack/msgpack-go"
+)
+
+var (
+	device      string = "lo0"
+	snaplen     int32  = 1024
+	promiscuous bool   = false
+)
+
+func main() {
+	handle, err := pcap.OpenLive(device, snaplen, promiscuous, pcap.BlockForever)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer handle.Close()
+
+	if err := handle.SetBPFFilter("tcp and port 24224"); err != nil {
+		panic(err)
+	}
+
+	packetSource := gopacket.NewPacketSource(handle, handle.LinkType())
+	for packet := range packetSource.Packets() {
+		handlePacket(packet)
+	}
+}
+
+func handlePacket(packet gopacket.Packet) {
+	if app := packet.ApplicationLayer(); app != nil {
+		fmt.Println("Application layer/Payload found.")
+		v, _, _ := msgpack.Unpack(bytes.NewReader(app.Payload()))
+		fmt.Printf("%s\n", v)
+	}
+}
